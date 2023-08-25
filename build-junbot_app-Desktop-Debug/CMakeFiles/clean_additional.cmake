caeb2ef2e15c4ec80a63191e4e685a7525f40340
lacie <00sao00ios00@gmail.com>
2023-08-25 12:01:00 +0700

    [Clean code]

--- build-junbot_app-Desktop-Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/junbot_app_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/junbot_app_autogen.dir/ParseCache.txt"
-  "junbot_app_autogen"
-)
