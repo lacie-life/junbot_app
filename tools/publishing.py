3bf404fdebe3294c26fbe113cd4be5a56e1b7a75
Life Zero <55802547+lacie-life@users.noreply.github.com>
2023-11-05 14:41:23 +0900

    Update publishing.py

--- tools/publishing.py
+++ tools/publishing.py
@@ -5,10 +5,22 @@ import time
 import json
 from paho.mqtt import client as mqtt_client
 
-
+# Broker address
 broker = 'localhost'
+
+# Broker port
 port = 1883
-topic = "sensor/001"
+
+# Topic publish
+topic_state = "robot1/state"
+topic_login_userInforesponse = "robot1/login_userInforesponse"
+topic_deliver_response = "robot1/deliver_response"
+
+# Topic subscribe
+topic_control = "robot1/control"
+topic_login_request = "robot1/login_request"
+topic_deliver = "robot1/deliver"
+
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
@@ -22,109 +34,108 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    client.username_pw_set("nam", "1")
+    
+    # Update username and password if it is set by your admin
+    client.username_pw_set("Demo", "1")
     client.on_connect = on_connect
     client.connect(broker, port)
+
+    client.subscribe(topic_control)
+    client.subscribe(topic_login_request)
+    client.subscribe(topic_deliver)
+
     return client
 
+def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
 def publish(client):
-    id = 0
-    tem = 0
-    humidity = 0
-    pest_counting = 0
-    time_repeat = "day"
-    time_start = "07:00:00"
-    state = "off"
-    month = "12"
-    day = "12"
-    hour = "12"
-    minute = "12"
-    msg_count = {
-    "name_gate" : "farm1",
-    "id_device" : {
-        "type" : "LoRa",
-        "group" : "A",
-        "index_node" : 1
-    },
-    "data" : {
-        "id": id,
-        "time" : {
-            "mm" : month,
-            "dd" : day,
-            "h" : hour,
-            "m" : minute
-        },
-        "tem": tem,
-        "humidity": humidity,
-        "pest_counting" : pest_counting
-    },
-    "state_pump" :
-        {
-        "state" : state,
-        "time_execution" : {
-            "time_repeat" : time_repeat,
-            "time_start" : time_start
+
+    # default msg
+    msg_control = {
+        "control": "turn right"
+    }
+
+    msg_state = {
+        "battery": 100,
+        "battery_state": 0,
+        "is_controlling_state": 0,
+        "is_mission_state": 0,
+        "sensor_state": 0,
+        "time": "2020.12.12 12:12:12"
+    }
+
+    msg_login_request = {
+        "username": "Demo",
+        "password": "1"
+    }
+
+    msg_login_userInforesponse = {
+        "response": "success", # or "fail"
+        "userInfo": {
+            "username": "Demo",
+            "password": "1",
+            "name": "Nam",
+            "phone": "123456789",
+            "email": ""
         }
     }
+
+    msg_deliver = {
+        "target": {
+            "name": "A",
+            "x": 0,
+            "y": 0,
+            "z": 0
+        },
+        "target": {
+            "name": "B",
+            "x": 1,
+            "y": 1,
+            "z": 1
+        },
+        "target": {
+            "name": "C",
+            "x": 2,
+            "y": 2,
+            "z": 2
+        }
     }
+
+    msg_deliver_response = {
+        "response": "success" # or "fail"
+        # "target": {
+        #     "name": "A",
+        #     "x": 0,
+        #     "y": 0,
+        #     "z": 0
+        # }
+    }
+
+
     while True:
         time.sleep(5)
-        # msg = f"messages: {msg_count}"
-        msg = msg_count
-        # print(type(msg))
+
+        msg = msg_state
         msg = json.dumps(msg)
-        result = client.publish(topic, msg)
-        # result: [0, 1]
+        result = client.publish(topic_state, msg)
         status = result[0]
         if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
+            print(f"Send `{msg}` to topic `{topic_state}`")
         else:
-            print(f"Failed to send message to topic {topic}")
-        id += 1
-        tem = random.randint(5, 15)
-        humidity = random.randint(5, 15)
-        pest_counting = random.randint(1,5)
-        month = "12"
-        day = "12"
-        hour = "12"
-        minute = "12"
-        msg_count = {
-            "name_gate": "farm1",
-            "id_device": {
-                "type": "LoRa",
-                "group": "A",
-                "index_node": 1
-            },
-            "data": {
-                "id": id,
-                "time": {
-                    "mm": month,
-                    "dd": day,
-                    "h": hour,
-                    "m": minute
-                },
-                "tem": tem,
-                "humidity": humidity,
-                "pest_counting": pest_counting
-            },
-            "state_pump":
-                {
-                    "state": state,
-                    "time_execution": {
-                        "time_repeat": time_repeat,
-                        "time_start": time_start
-                    }
-                }
-        }
+            print(f"Failed to send message to topic {topic_state}")
+
         
 
 def run():
     client = connect_mqtt()
+    client.on_message = on_message
     client.loop_start()
     publish(client)
 
 
 if __name__ == '__main__':
     run()
+
 
 
